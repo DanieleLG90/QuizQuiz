6396e88c3de0c4ff8efef4c6f560d533511b48f3
Daniele Lorenzo Giacomazzi <giacomazzi06@gmail.com>
2025-08-12 18:57:33 +0200

    question page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
-import { useState } from 'react'
+import React from 'react'
 
 import Intro from "./components/Intro"
+import Question from "./components/Question"
 
 import './App.css'
 
 function App() {
+
+  const [quizStart, setQuizStart] = React.useState(false)
+
+    function go() {
+      setQuizStart(true)
+    }
   
   return (
     <>
      <h1>Quiz Quiz Game</h1>
-     <Intro />
+     {!quizStart?
+     <Intro startQuiz={go} />: 
+
+     <div>
+      <h2>answer all the questions below: </h2>
+      <div>
+          <Question />
+          <Question />
+          <Question />
+          <Question />
+          <Question />
+        </div>
+     </div>
+
+     }
     </>
   )
 }
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,4 +1,24 @@
 import React from "react"
 
 
+export default function(props){
+    /*
+    const [selectedAnswer, setSelectedAnswer] = React.useState(null)
 
+    function handleClick(answerText){
+        setSelectedAnswer(answerText)
+    }
+     */
+
+    return(
+        <div>
+            <h3>Question</h3>
+            <div>
+                <p>answer1</p>
+                <p>answer2</p>
+                <p>answer3</p>
+                <p>answer4</p>
+            </div>
+        </div>
+    )
+}
